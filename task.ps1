01ed9dcd82b77a2e58084978610cd9a35f1957f8
Eugene Ermakov <eugeniy696@gmail.com>
2026-06-16 21:19:11 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,3 @@
-# Write your code here
+$list = Get-AzDisk
+$listOfUnattachedDisks = $list | Where-Object { $_.ManagedBy -eq $null }
+$listOfUnattachedDisks | ConvertTo-Json | Out-File -Path ./result.json
